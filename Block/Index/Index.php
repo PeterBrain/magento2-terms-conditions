353c0130d038b8027fa563eac44387aa5e34b084
PeterBrain <peter.loecker@live.at>
2020-04-19 12:40:22 +0200

    remove old code

--- Block/Index/Index.php
+++ Block/Index/Index.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace PeterBrain\TermsConditions\Block\Index;
 
-
 class Index extends \Magento\Framework\View\Element\Template
 {
+    protected $_agreements;
 
     /**
      * Constructor
@@ -16,9 +16,17 @@ class Index extends \Magento\Framework\View\Element\Template
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\CheckoutAgreements\Model\Agreement $agreements,
         array $data = []
     ) {
+        $this->_agreements = $agreements;
         parent::__construct($context, $data);
+    }
+
+    public function getAgreementById($agreementId)
+    {
+        $agree = $this->_agreements->load($agreementId);
+        return $agree->getContent();
     }
 }
 
